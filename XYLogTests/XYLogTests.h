bb64a8abad6874d1ac312aece95e4fe103159aac
lishuan yang <2008.yls@163.com>
2013-07-01 11:49:12 +0800

    Initial Commit

---
+++ XYLogTests/XYLogTests.h
@@ -0,0 +1,13 @@
+//
+//  XYLogTests.h
+//  XYLogTests
+//
+//  Created by yls on 13-7-1.
+//  Copyright (c) 2013年 yls. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XYLogTests : SenTestCase
+
+@end
---
+++ XYLogTests/XYLogTests.m
@@ -0,0 +1,32 @@
+//
+//  XYLogTests.m
+//  XYLogTests
+//
+//  Created by yls on 13-7-1.
+//  Copyright (c) 2013年 yls. All rights reserved.
+//
+
+#import "XYLogTests.h"
+
+@implementation XYLogTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XYLogTests");
+}
+
+@end
